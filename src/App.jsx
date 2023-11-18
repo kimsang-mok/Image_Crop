955f010d974c1b8077dbb3c89004117498b66744
kimsang-lov <kimsang.mok01@gmail.com>
2023-11-18 21:46:53 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,133 @@
+import { useState, useEffect } from "react";
+import ReactCrop from "react-image-crop";
+import { useDropzone } from "react-dropzone";
+
+const thumbsContainer = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16,
+  padding: 20,
+};
+
+const thumb = {
+  position: "relative",
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: 400,
+  height: 300,
+  padding: 4,
+  boxSizing: "border-box",
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden",
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
+
+const thumbButton = {
+  position: "absolute",
+  right: 10,
+  bottom: 10,
+};
+
+function App() {
+  const [files, setFiles] = useState([]);
+  const [crop, setCrop] = useState();
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const getCroppedImg = (image, crop, fileName) => {
+    const canvas = document.createElement("canvas");
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob((blob) => {
+        if (!blob) {
+          console.error("Canvas is empty");
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+      }, "image/jpeg");
+    });
+  };
+
+  const thumbs = files.map((file, index) => (
+    <div style={thumb} key={file.name}>
+      {/* <div style={thumbInner}>
+        <img src={file.preview} style={img} alt="" />
+      </div> */}
+      <ReactCrop crop={crop} onChange={(c) => setCrop(c)}>
+        <img src={file.preview} />
+      </ReactCrop>
+
+      <button
+        style={thumbButton}
+        onClick={() => {
+          console.log("Hi");
+        }}
+      >
+        Edit
+      </button>
+    </div>
+  ));
+
+  useEffect(
+    () => () => {
+      // Make sure to revoke the Object URL to avoid memory leaks
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
+
+  return (
+    <section className="container">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside style={thumbsContainer}>{thumbs}</aside>
+    </section>
+  );
+}
+
+export default App;
